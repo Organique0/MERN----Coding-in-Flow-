934ad549e5341feaefbc753408d9ebb456cec780
Organique0 <grabnar.luka@gmail.com>
2023-05-01 22:55:06 +0200

    zacetek React.js

---
+++ frontend/src/components/Note.tsx
@@ -0,0 +1,32 @@
+import { Note as NoteModel } from "../models/note";
+import { Card } from "react-bootstrap";
+import styles from "../styles/Note.module.css";
+import { format } from "path";
+import { formatDate } from "../util/formatDate";
+
+interface NoteProps {
+  note: NoteModel;
+  className?: string;
+}
+
+const Note = ({ note, className }: NoteProps) => {
+  const { title, text, createdAt, updatedAt } = note;
+
+  let createdUpdatedText: string;
+  if (updatedAt > createdAt) {
+    createdUpdatedText = "Updated " + formatDate(updatedAt);
+  } else {
+    createdUpdatedText = "created " + formatDate(createdAt);
+  }
+  return (
+    <Card className={`${styles.noteCard} ${className}`}>
+      <Card.Body className={styles.cardBody}>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text className={styles.cardText}>{text}</Card.Text>
+      </Card.Body>
+      <Card.Footer className="text-muted">{createdUpdatedText}</Card.Footer>
+    </Card>
+  );
+};
+
+export default Note;
