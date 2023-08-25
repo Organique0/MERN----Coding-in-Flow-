b1ffba1d79efa68bc2bcba4491ce4f0222180ff2
Organique0 <grabnar.luka@gmail.com>
2023-08-25 20:30:54 +0200

    f

--- frontend/src/network/notes_api.ts
+++ frontend/src/network/notes_api.ts
@@ -3,7 +3,7 @@ import { Note } from "../models/note";
 import { User } from "../models/user";
 
 async function fetchData(input: RequestInfo, init?: RequestInit) {
-  const response = await fetch("https://cool-notes-app-d547271d81da.herokuapp.com" + input, init); //for localhost remove this url and add a proxy back
+  const response = await fetch(input, init); //for localhost remove this url and add a proxy back
   if (response.ok) {
     return response;
   } else {
