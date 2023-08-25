c960f1da6ea7fd3b90636f236d931ed0ddacfe75
Organique0 <grabnar.luka@gmail.com>
2023-08-25 20:35:02 +0200

    yes cors is very fun

--- src/app.ts
+++ src/app.ts
@@ -18,6 +18,8 @@ app.use(express.json());
 app.use(cors({
   origin: "https://mern-notes-app-rouge.vercel.app",
   credentials: true,
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  allowedHeaders: "Content-Type,Authorization",
 }));
 app.use(
   session({
