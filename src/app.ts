6bced9079bb759e65a7fbe3b78159b6440620947
Organique0 <grabnar.luka@gmail.com>
2023-08-25 22:06:46 +0200

    a problem only caused by my stupidity

--- src/app.ts
+++ src/app.ts
@@ -16,23 +16,15 @@ app.use(morgan("dev"));
 
 app.use(express.json());
 
-const allowCrossDomain = (req: Request, res: Response, next: NextFunction) => {
-  res.header(`Access-Control-Allow-Origin`, `https://mern-notes-app-rouge.vercel.app`);
-  res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`);
-  res.header(`Access-Control-Allow-Headers`, `Content-Type`);
-  next();
-};
-
-
-/* const corsOptions: CorsOptions = {
+const corsOptions: CorsOptions = {
   origin: 'https://mern-notes-app-rouge.vercel.app',
   methods: ['POST', 'GET', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
+};
 
-}
-app.use(cors(corsOptions)); */
-app.use(allowCrossDomain);
+app.use(cors(corsOptions));
+
 app.use(
   session({
     secret: env.SESSION_SECRET,
