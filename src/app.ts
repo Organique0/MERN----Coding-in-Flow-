3fb7f69e2cb7455bd49c469781d2ff1f53d1b681
Organique0 <grabnar.luka@gmail.com>
2023-08-25 21:02:47 +0200

    dam

--- src/app.ts
+++ src/app.ts
@@ -15,11 +15,12 @@ const app = express();
 app.use(morgan("dev"));
 
 app.use(express.json());
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+const corsOptions = {
+  origin: 'https://mern-notes-app-rouge.vercel.app',
+  methods: ['POST', 'GET', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}
+app.use(cors(corsOptions));
 app.use(
   session({
     secret: env.SESSION_SECRET,
